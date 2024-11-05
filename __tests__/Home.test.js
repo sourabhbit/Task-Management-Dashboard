0b64313a77cac36f0ddb72fff8da08c0dd29ac7b
sourabh kumar <sourabh.kumar@stablemoney.in>
2024-11-06 02:46:11 +0530

    testing setup

--- __tests__/Home.test.js
+++ __tests__/Home.test.js
@@ -1,8 +1,8 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import Home from "../pages/index";
-import { v4 as uuidv4 } from "uuid";
-import localStorageHelper from "../utils/helper";
 import "@testing-library/jest-dom";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { v4 as uuidv4 } from "uuid";
+import Home from "../pages/index";
+import localStorageHelper from "../utils/helper";
 
 jest.mock("../utils/helper");
 
@@ -32,15 +32,14 @@ describe("Home Component", () => {
   ];
 
   beforeEach(() => {
-    // Mock the localStorage helper
     localStorageHelper.getItem = jest.fn().mockReturnValue(mockTasks);
   });
 
   test("renders Home page correctly", async () => {
     render(<Home />);
-    expect(screen.getByPlaceholderText("Search Task")).toBeInTheDocument(); // Check the input for search task
-    expect(screen.getByText("Sort By Due Date")).toBeInTheDocument(); // Check for Sort button
-    expect(screen.getByText("Board View")).toBeInTheDocument(); // Check for Board View toggle
+    expect(screen.getByPlaceholderText("Search Task")).toBeInTheDocument();
+    expect(screen.getByText("Sort By Due Date")).toBeInTheDocument();
+    expect(screen.getByText("Board View")).toBeInTheDocument();
   });
 
   test("handles search input and filters tasks", async () => {
@@ -62,20 +61,18 @@ describe("Home Component", () => {
     const select = screen.getByTestId("select-element");
     fireEvent.change(select, { target: { value: "inprogress" } });
 
-    // Ensure the tasks are filtered based on the selected status
-    expect(screen.getByText("Task 2")).toBeInTheDocument(); // Task 2 should be visible
-    expect(screen.queryByText("Task 1")).not.toBeInTheDocument(); // Task 1 should be hidden
-    expect(screen.queryByText("Task 3")).not.toBeInTheDocument(); // Task 3 should be hidden
+    expect(screen.getByText("Task 2")).toBeInTheDocument();
+    expect(screen.queryByText("Task 1")).not.toBeInTheDocument();
+    expect(screen.queryByText("Task 3")).not.toBeInTheDocument();
   });
 
   test("sorts tasks based on due date", async () => {
     render(<Home />);
 
     const sortButton = screen.getByText("Sort By Due Date");
-    fireEvent.click(sortButton); // Sort in ascending order first
+    fireEvent.click(sortButton);
 
-    // Ensure the tasks are sorted by the due date
-    const sortedTasks = await screen.findAllByText(/Task \d+/); // The task titles
+    const sortedTasks = await screen.findAllByText(/Task \d+/);
     expect(sortedTasks[0].textContent).toBe("Task 1");
     expect(sortedTasks[1].textContent).toBe("Task 2");
     expect(sortedTasks[2].textContent).toBe("Task 3");
